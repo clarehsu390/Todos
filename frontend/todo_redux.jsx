ca7eda93ba00643993929f696e726614b3f8d08b
Clare Hsu <clare.hsu390@gmail.com>
2018-03-26 14:09:31 -0700

    set up store

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,7 +1,10 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded',() => {
     let content = document.getElementById('content');
+    let store = configureStore();
+    window.store = store;
     ReactDOM.render(<h1>Todos App</h1>, content);
 });
